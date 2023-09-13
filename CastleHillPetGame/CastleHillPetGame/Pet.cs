a65d29856ebbcd42aa7e5596e84d0c7e331f86e0
Dominic James <30402312+dom3691@users.noreply.github.com>
2023-09-13 23:38:51 +0100

    addidng cat and dog classs

--- CastleHillPetGame/CastleHillPetGame/Pet.cs
+++ CastleHillPetGame/CastleHillPetGame/Pet.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml.Linq;
 
 namespace CastleHillPetGame
 {
@@ -14,10 +13,11 @@ namespace CastleHillPetGame
         public int Happiness { get; set; }
         public int MaxHunger { get; }
         public int MaxHappiness { get; }
+        public string Dog { get; set; }
 
         public Pet(string name, int maxHunger, int maxHappiness)
         {
-            Name = name;
+            Name = Dog;
             MaxHunger = maxHunger;
             MaxHappiness = maxHappiness;
             Hunger = 2; // Starting hunger level
@@ -27,36 +27,18 @@ namespace CastleHillPetGame
         // Implement methods for feeding and interacting with pets
         public void Feed(string food)
         {
-            Pet petToFeed = pets.FirstOrDefault(p => p.Name == name);
-            if (petToFeed != null)
-            {
-                petToFeed.Feed(food);
-                Console.WriteLine($"{petToFeed.Name} has been fed {food}.");
-            }
-            else
-            {
-                Console.WriteLine($"{name} not found in your collection.");
-            }
+            // Handle food logic here
         }
 
         public void Interact(string action)
         {
-            Pet petToInteract = pets.FirstOrDefault(p => p.Name == name);
-            if (petToInteract != null)
-            {
-                petToInteract.Interact(action);
-                Console.WriteLine($"{petToInteract.Name} has been interacted with using {action}.");
-            }
-            else
-            {
-                Console.WriteLine($"{name} not found in your collection.");
-            }
+            // Handle interaction logic here
         }
 
         // Implement a method to update pet stats over time
         public void UpdateStats()
         {
             // Handle stat updates over time (e.g., increase hunger, decrease happiness)
-        }   
+        }
     }
 }
---
+++ CastleHillPetGame/CastleHillPetGame/PetCollection.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CastleHillPetGame
+{
+    public class PetCollection
+    {
+        private List<Pet> pets;
+
+        public PetCollection()
+        {
+            pets = new List<Pet>();
+        }
+
+        public void AcquirePet(string name, string type)
+        {
+            if (type == "Dog")
+            {
+                pets.Add(new Dog(name));
+            }
+            else if (type == "Cat")
+            {
+                pets.Add(new Cat(name));
+            }
+        }
+
+        public void RemovePet(string name)
+        {
+            // Remove a pet from the collection by name
+        }
+
+        public void ListPets()
+        {
+            // Display a list of pets in the collection
+        }
+
+        public void PlayWithPets(string pet1Name, string pet2Name)
+        {
+            // Implement logic for pets playing together
+        }
+
+        public void ViewPetStats(string name)
+        {
+            // Display the current state of a pet
+        }
+    }
+}
